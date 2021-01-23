8b2a416e31a819695b83b341877cda18d0eb2521
Isaac Johnson <isaac.johnson.2493@gmail.com>
2021-01-22 18:28:53 -0700

    Removed hero image

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,12 @@
 import AboutContent from '../components/AboutContent';
 import AboutMe from '../components/AboutMe';
-import PageHero from '../components/AboutHero';
+// import PageHero from '../components/AboutHero';
 import Services from '../components/Services';
 
 export default function About() {
   return (
     <>
-      <PageHero />
+      {/* <PageHero /> */}
       <AboutMe />
       <AboutContent />
       <Services />
