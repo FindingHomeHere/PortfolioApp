01500a6844eba76cd58695c810a346be220fc1b5
Isaac Johnson <isaac.johnson.2493@gmail.com>
2021-01-22 19:06:14 -0700

    Changed layout of about page

--- pages/about.js
+++ pages/about.js
@@ -8,8 +8,10 @@ export default function About() {
     <>
       {/* <PageHero /> */}
       <AboutMe />
-      <AboutContent />
-      <Services />
+      <div>
+        <AboutContent />
+        <Services />
+      </div>
     </>
   );
 }
